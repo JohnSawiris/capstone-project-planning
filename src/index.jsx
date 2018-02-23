3dfa9896db551a0f5a7751e01b008826a8672f1a
John Sawiris <johnedwardsawiris@gmail.com>
2018-02-23 11:46:08 -0800

    configure webpack bundling and dev server

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+//components
+import App from './components/App';
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('react-container')
+  );
+}
+
+render(App);
+
+if(module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  });
+}
